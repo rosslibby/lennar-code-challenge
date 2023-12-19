f5fdfb479da5c9a10d3bd5652ca1edebb01aaf86
Ross <dev@rosslibby.com>
2023-12-19 17:29:01 -0500

    added Main component to Wrapper

--- src/wrapper/index.tsx
+++ src/wrapper/index.tsx
@@ -1,7 +1,8 @@
 import Header from '../components/header'
+import Main from '../components/main'
 import styles from './wrapper.module.css'
 
-export default function Main() {
+export default function Wrapper() {
   return (
     <div className={styles.wrapper}>
       <Header />
