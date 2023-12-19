799358c020e5e86683a3697a50653bfcac32af21
Ross <dev@rosslibby.com>
2023-12-19 16:56:52 -0500

    built header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,17 @@
+import Nav from '../nav'
+import styles from './header.module.css'
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.leading}>
+        <img className={styles.logo} src="./assets/logo.svg" />
+        <Nav />
+      </div>
+      <div className={styles.trailing}>
+        <span>Log in</span>
+        <button>Start free trial</button>
+      </div>
+    </header>
+  )
+}
