f7cc1a2a308e95119d36b00d91582dfacc2af9b0
Ross <dev@rosslibby.com>
2023-12-19 16:29:44 -0500

    built base dom

---
+++ src/main/index.tsx
@@ -0,0 +1,39 @@
+import styles from './main.module.sass'
+
+export default function Main() {
+  return (
+    <div className={styles.main}>
+      <header>
+        <img src="./assets/logo.svg" alt="Lennar Code Challenge" />
+        <nav>
+          <ul>
+            <li>Product</li>
+            <li>Features</li>
+            <li>Marketplace</li>
+            <li>Company</li>
+          </ul>
+        </nav>
+        <span>Log in</span>
+        <button>Start free trial</button>
+      </header>
+      <main>
+        <div className={styles.hero}>
+          <div className={styles.heroCallout}>
+            <span className={styles.pill}>We're hiring</span>
+            <span>Visit our careers page</span>
+            <span>&rcar;</span>
+          </div>
+          <div className={styles.heroText}>
+            <h1>A better way to <span>ship web apps</span></h1>
+            <p>Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui Lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.</p>
+          </div>
+          <form>
+            <input type="email" placeholder="Enter your email" />
+            <button>Start free trial</button>
+            <p>Start your free 14-day trial, no credit card necessary. By providing your email, you agree to our <a href="#">terms of service</a>.</p>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
