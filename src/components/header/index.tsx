b4ffb3d23402f4a2267910daa1d0eb2438d26d5d
Ross <dev@rosslibby.com>
2023-12-19 19:39:48 -0500

    built mobile nav support

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,24 @@
+import { useState } from 'react'
 import { Button } from '../form/button'
 import { ButtonVariant } from '../form/types'
 import Nav from '../nav'
 import styles from './header.module.css'
 
 export default function Header() {
+  const [showNav, setShowNav] = useState<boolean>(false)
+  const toggleNav = () => setShowNav((state: boolean) => !state)
+
   return (
     <header className={styles.header}>
       <div className={styles.leading}>
         <img className={styles.logo} src="./assets/logo.svg" alt="Lennar Code Challenge" />
-        <span className={styles.mobileNav}>
+        <button className={styles.mobileNav} onClick={toggleNav}>
           <img src="./assets/hamburger.svg" />
-        </span>
-        <Nav />
+        </button>
+        {showNav && (
+          <div className={styles.navBackdrop} onClick={toggleNav} />
+        )}
+        <Nav toggle={toggleNav} visible={showNav} />
       </div>
       <div className={styles.trailing}>
         <Button
--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from '../form/button'
+import { ButtonVariant } from '../form/types'
 import NavItem from './item'
 import styles from './nav.module.css'
 
@@ -20,12 +22,36 @@ const navItems = [
   },
 ]
 
-export default function Nav() {
+export default function Nav({ toggle, visible }: {
+  toggle: () => void
+  visible: boolean
+}) {
+  const navClassNames = [styles.nav, ...(
+    visible
+      ? [styles.visible]
+      : []
+    )
+  ].join(' ')
+
   return (
-    <nav className={styles.nav}>
+    <nav className={navClassNames}>
+      <div className={styles.navHeader}>
+        <img src="./assets/logo.svg" />
+        <button className={styles.closeNav} onClick={toggle}>
+          <img src="./assets/close.svg" />
+        </button>
+      </div>
       {navItems.map((item) => (
         <NavItem key={item.url} {...item} />
       ))}
+      <div className={styles.navFooter}>
+        <Button
+          label="Start free trial"
+          onClick={() => null}
+          variant={ButtonVariant.primary}
+        />
+        <span>Existing customer? <a href="/login">Login</a></span>
+      </div>
     </nav>
   )
 }
